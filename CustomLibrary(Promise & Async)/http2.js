1f42a7a3a86073613872b72b17bbbe82155cc943
Ashish911 <50243429+Ashish911@users.noreply.github.com>
2021-03-02 20:48:49 +0545

    Add files via upload
    almost done

---
+++ CustomLibrary(Promise & Async)/http2.js
@@ -0,0 +1,53 @@
+/**
+ * @author Ashish Dongol
+ */
+
+class HTTP {
+
+    // Get request
+    async get(url) {
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
+    }
+
+    // Make Post request
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const resData = await response.json();
+        return resData;
+    }
+
+    // Make PUT request
+    async put(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const resData = await response.json();
+        return resData;
+    }
+
+    // Make DELETE request
+    async delete(url) {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        const resData = await "Resource deleted";
+        return resData;
+    }
+
+}
